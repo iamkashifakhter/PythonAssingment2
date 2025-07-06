939650c19fba98838214d5f869326f817c24cfef
Kashif Akhter <146735547+iamkashifakhter@users.noreply.github.com>
2025-07-06 10:21:09 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,13 @@
+'''
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+
+'''
+sum = 0
+for i in range(51):
+    sum = sum + i
+print("The sum of number from 1 to 50 is: ",sum)
+
+
